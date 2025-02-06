826dd2c98e8fdd70560b015091ece247b71d5850
Ra1ner <def1x.yt@yandex.ru>
2025-02-07 01:39:09 +0300

    Refactoring and changing something functions

--- main.go
+++ main.go
@@ -7,34 +7,41 @@ import (
     "time"
 )
 
+const (
+    letters         = "QWERTYUIOPASDFGHJKLZXCVBNM"
+    maxOrderLength  = 8
+    useOfHashtag    = true // Использовать # в начале?
+    
+    TYPE_DEV    = "dev"
+    TYPE_DESIGN = "design"
+)
+
+var ordersType = map[string]string{
+    TYPE_DEV:    "DEV",
+    TYPE_DESIGN: "DESIGN",
+}
+
+// randBool возвращает случайное булево значение
 func randBool() bool {
     return rand.Intn(2) == 1
 }
 
+// randRange возвращает случайное число в заданном диапазоне [min, max)
 func randRange(min, max int) int {
     return rand.Intn(max-min) + min
 }
 
-const letters string = "QWERTYUIOPASDFGHJKLZXCVBNM"
-const maxOrderLength int = 8
-const useOfHashtag bool = true // Is use # in starts?
-
-var ordersType map[string]string = map[string]string{
-    "dev" : "DEV",
-    "design" : "DESIGN",
-};
-
+// GenerateCode генерирует случайный код
 func GenerateCode() string {
     result := ""
-    
+
     if useOfHashtag {
         result += "#"
     }
-    
-    result += strconv.Itoa(randRange(0, 10));
-    for i := 0; i < maxOrderLength; i++ { //
-        isUseHereNumber := randBool()
-        if isUseHereNumber {
+
+    result += strconv.Itoa(randRange(0, 10))
+    for i := 0; i < maxOrderLength; i++ {
+        if randBool() {
             randomLetter := letters[randRange(0, len(letters))]
             result += string(randomLetter)
         } else {
@@ -44,19 +51,22 @@ func GenerateCode() string {
     return result
 }
 
-func AddingOrderTypeInResult(typeOrder string) string {
-    valueType, ok := ordersType[typeOrder];
-    if !ok  {
-        panic("Not founded: " + typeOrder);
+// SelectOrderType возвращает тип заказа по его строковому представлению
+func SelectOrderType(typeOrder string) string {
+    valueType, ok := ordersType[typeOrder]
+    if !ok {
+        panic("Не найден тип: " + typeOrder)
     }
-    
-    return fmt.Sprintf("%s-%s", valueType, GenerateCode())
-    
+    return valueType
 }
 
 func main() {
-    rand.Seed(time.Now().UnixNano()) // Seed the random number generator once
+    rand.Seed(time.Now().UnixNano()) // Инициализация генератора случайных чисел
+
     for a := 0; a < 50; a++ {
-        fmt.Printf("[%d] %s\n", a, AddingOrderTypeInResult("dev"))
+        randomTypes := []string{TYPE_DEV, TYPE_DESIGN}
+        randomType := randomTypes[randRange(0, len(randomTypes))] // Исправлено на randomTypes
+
+        fmt.Printf("[%d] %s\n", a, fmt.Sprintf("%s-%s", SelectOrderType(randomType), GenerateCode()))
     }
 }
