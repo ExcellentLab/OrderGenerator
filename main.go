04de26a5a8379b8c29ae235284f0b3ed247a621c
Ra1ner <def1x.yt@yandex.ru>
2025-02-06 20:44:07 +0300

    Adding more numbers

--- main.go
+++ main.go
@@ -4,32 +4,43 @@ import (
     "fmt"
     "math/rand"
     "strconv"
+    "time"
 )
+
+func randBool() bool {
+    return rand.Intn(2) == 1
+}
 
 func randRange(min, max int) int {
     return rand.Intn(max-min) + min
 }
 
-const letters string = "QWERTYUIOPASDFGHJKLZXCVBNM";
-const maxOrderLenght int = 8;
-const useOfHashtag bool = false; // Is use # in starts?
+const letters string = "QWERTYUIOPASDFGHJKLZXCVBNM"
+const maxOrderLength int = 8
+const useOfHashtag bool = true // Is use # in starts?
 
 func GenerateCode() string {
-    result := "#";
+    result := ""
     
-    if !useOfHashtag {
-        result = "";
-    };
+    if useOfHashtag {
+        result += "#"
+    }
     
-    result += strconv.Itoa(randRange(0, 9));
-    for i := 0; i < maxOrderLenght; i++ {
-        randomLetter := letters[randRange(0, len(letters))]
-        result += string(randomLetter)
+    result += strconv.Itoa(randRange(0, 10));
+    for i := 0; i < maxOrderLength; i++ { //
+        isUseHereNumber := randBool()
+        if isUseHereNumber {
+            randomLetter := letters[randRange(0, len(letters))]
+            result += string(randomLetter)
+        } else {
+            result += strconv.Itoa(randRange(0, 10))
+        }
     }
     return result
 }
 
 func main() {
+    rand.Seed(time.Now().UnixNano()) // Seed the random number generator once
     for a := 0; a < 2; a++ {
         fmt.Printf("Generated: %s\n", GenerateCode())
     }
