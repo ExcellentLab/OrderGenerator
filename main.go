d024055850ea4f10f1a3c3673f696c4e7374bd5b
Ra1ner <def1x.yt@yandex.ru>
2025-02-06 20:28:00 +0300

    Adding check to # in starts of result string

--- main.go
+++ main.go
@@ -12,9 +12,15 @@ func randRange(min, max int) int {
 
 const letters string = "QWERTYUIOPASDFGHJKLZXCVBNM";
 const maxOrderLenght int = 8;
+const useOfHashtag bool = false; // Is use # in starts?
 
 func GenerateCode() string {
-    result := "#"
+    result := "#";
+    
+    if !useOfHashtag {
+        result = "";
+    };
+    
     result += strconv.Itoa(randRange(0, 9));
     for i := 0; i < maxOrderLenght; i++ {
         randomLetter := letters[randRange(0, len(letters))]
@@ -24,8 +30,6 @@ func GenerateCode() string {
 }
 
 func main() {
-    /*fmt.Println(letters)
-    fmt.Println(maxOrderLenght)*/
     for a := 0; a < 2; a++ {
         fmt.Printf("Generated: %s\n", GenerateCode())
     }
